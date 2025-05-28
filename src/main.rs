0614556b90ea829d97919e5af209c71bb699bb07
Sasu <sipolansasu@gmail.com>
2025-05-28 13:13:54 +0300

    Changed port

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> std::io::Result<()> {
             .route("/hello", web::get().to(hello))
             .route("/echo", web::post().to(echo))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:8081")?
     .run()
     .await
 }
